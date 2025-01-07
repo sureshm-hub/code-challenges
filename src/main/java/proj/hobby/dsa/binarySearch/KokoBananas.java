62adada35a082c9155b4bd18534cbc48cdb555be
sures <suresh.malladi@gmail.com>
2025-01-06 22:44:22 -0500

    Koko Bananas

--- src/main/java/proj/hobby/dsa/binarySearch/KokoBananas.java
+++ src/main/java/proj/hobby/dsa/binarySearch/KokoBananas.java
@@ -14,7 +14,7 @@ public class KokoBananas {
 
     public int minEatingSpeed(int[] piles, int h) {
         int lo = 1, hi = 0;// lo, hi to track the values of k
-        int loHrs = 0;
+        long loHrs = 0; //long to avoid integer overflow
         for(int p : piles) {
             loHrs += p;
             hi = Math.max(hi, p);
@@ -46,5 +46,6 @@ public class KokoBananas {
         System.out.println("min Eating Speed: "+kb.minEatingSpeed(new int[]{30,11,23,4,20}, 6));
         System.out.println("min Eating Speed: "+kb.minEatingSpeed(new int[]{300}, 6));
         System.out.println("min Eating Speed: "+kb.minEatingSpeed(new int[]{299}, 6));
+        System.out.println("min Eating Speed: "+kb.minEatingSpeed(new int[]{332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184}, 823855818));
     }
 }
