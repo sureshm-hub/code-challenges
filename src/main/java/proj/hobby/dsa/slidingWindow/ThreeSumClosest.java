8a8a3d993c4c166fdf40a70024058f5273bc386a
sures <suresh.malladi@gmail.com>
2025-08-24 22:32:17 -0400

    Three Sum Closest

---
+++ src/main/java/proj/hobby/dsa/slidingWindow/ThreeSumClosest.java
@@ -0,0 +1,31 @@
+package proj.hobby.dsa.slidingWindow;
+
+import java.util.*;
+
+/**
+ *
+ * https://leetcode.com/problems/3sum-closest/
+ *
+ */
+public class ThreeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        System.out.println("nums: "+Arrays.toString(nums));
+        int result =nums[0]+nums[1]+nums[2];
+        int min = Math.abs(target-result);
+
+        for(int i=0; i < nums.length-2; i++) {
+            for(int j = i+1; j < nums.length-1; j++) {
+                for(int k = j+1; k < nums.length; k++) {
+                    int curr = nums[i]+nums[j]+nums[k];
+                    int currMin = Math.abs(target-curr);
+                    if(currMin < min) {
+                        result = curr;
+                        min = currMin;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
