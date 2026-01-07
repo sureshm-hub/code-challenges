46df28323b1c48c1f1405d20e70936b752c6daa8
sures <suresh.malladi@gmail.com>
2026-01-07 07:43:48 -0500

    isomorphic

--- src/main/java/proj/hobby/dsa/strings/IsomorphicStrings.java
+++ src/main/java/proj/hobby/dsa/strings/IsomorphicStrings.java
@@ -11,6 +11,38 @@ import java.util.Map;
 public class IsomorphicStrings {
 
     public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        int[] arr1 = new int[256];
+        int[] arr2 = new int[256];
+        for(int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            // only checks for frequency not where the character appears
+            // arr1[c1]++;
+            // arr2[c2]++;
+            // if(arr1[c1] != arr2[c2]) return false;
+
+            /**
+             *
+             * 0 means not seen before. At position i, if the previous occurrence index of c1 in s is not the same as
+             * the previous occurrence index of c2 in t, their repetition pattern differs, so the strings cannot be
+             * isomorphic.
+             *
+             * effectively encodes the pattern “first time / second time / third time
+             *
+             */
+            if(arr1[c1] != arr2[c2]) return false;
+
+            arr1[c1] = i + 1;
+            arr2[c2] = i + 1;
+        }
+
+        return true;
+    }
+
+    public boolean isIsomorphic2(String s, String t) {
         if(s == null || t == null) return s == null && t == null;
         return getIso(s).equals(getIso(t));
     }
