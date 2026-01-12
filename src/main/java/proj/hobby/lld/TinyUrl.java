1e4751f4ad3ba2ffaf855f841c4776009e2a097a
sures <suresh.malladi@gmail.com>
2026-01-12 00:25:50 -0500

    refactoring

--- src/main/java/proj/hobby/lld/TinyUrl.java
+++ src/main/java/proj/hobby/lld/TinyUrl.java
@@ -1,4 +1,4 @@
-package proj.hobby.dsa.maps;
+package proj.hobby.lld;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,8 +9,12 @@ import java.util.Random;
  * https://leetcode.com/problems/encode-and-decode-tinyurl
  *
  * Code Space Size: 62 ^ 6 ~ 56 Billion Url's
- * URL Spae: > 10 ^ 100
- * Probability of Collision: 1.25 * SQRT(62 ^6) ~ 300K  (50% probability of at least 1 collision)
+ * URL Spae: > 10 ^ 100  -> vastly exceeds code space
+ * It is ok that code space is tiny compared to the url space because we are not mapping all possible URL's at once
+ *
+ * Probability of Collision "Birthday Paradox": 1.25 * SQRT(62 ^6) ~ 300K  (50% probability of at least 1 collision)
+ * It's ok as collisions are handled by retries
+ *
  * https://chatgpt.com/g/g-p-67c12b389aec81918518492065db0483-dsa/c/692d02c7-bfa0-8333-bbd7-dae43a64f2ae
  *
  */
