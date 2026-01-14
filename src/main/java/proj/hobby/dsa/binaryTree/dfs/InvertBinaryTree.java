f40f74612f9c3c99f9cadff5cc9229ef8eac14be
sures <suresh.malladi@gmail.com>
2026-01-13 23:24:02 -0500

    refactoring

--- src/main/java/proj/hobby/dsa/binaryTree/dfs/InvertBinaryTree.java
+++ src/main/java/proj/hobby/dsa/binaryTree/dfs/InvertBinaryTree.java
@@ -11,18 +11,28 @@ public class InvertBinaryTree {
 
     public TreeNode invertTree(TreeNode root) {
         if(root == null) return null;
-        return invert(root, new TreeNode(root.val));
+
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+
+        return root;
     }
 
-    private TreeNode invert(TreeNode orig, TreeNode invert) {
+    public TreeNode invertTree2(TreeNode root) {
+        if(root == null) return null;
+        return invert2(root, new TreeNode(root.val));
+    }
+
+    private TreeNode invert2(TreeNode orig, TreeNode invert) {
 
         if(orig == null) return null;
 
         if (orig.left  != null) invert.right = new TreeNode(orig.left.val);
-        invert(orig.left, invert.right);
+        invert2(orig.left, invert.right);
 
         if (orig.right != null) invert.left = new TreeNode(orig.right.val);
-        invert(orig.right, invert.left);
+        invert2(orig.right, invert.left);
 
         return invert;
     }
