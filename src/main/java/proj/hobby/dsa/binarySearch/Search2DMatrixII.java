99ef0048e195fd5d35a8304b1194e18529e61d19
sures <suresh.malladi@gmail.com>
2026-01-17 00:34:41 -0500

    refactoring

--- src/main/java/proj/hobby/dsa/binarySearch/Search2DMatrixII.java
+++ src/main/java/proj/hobby/dsa/binarySearch/Search2DMatrixII.java
@@ -6,7 +6,31 @@ package proj.hobby.dsa.binarySearch;
  */
 public class Search2DMatrixII {
 
+
     public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        /**
+         * Why start at top‑right?
+         *
+         * Everything to the left in the same row ≤ the current value because the row is sorted.
+         * Everything below in the same column ≥ the current value because the column is sorted.
+         *
+         * So from this corner each comparison tells you whether to move left or down while safely discarding
+         * a whole strip of cells.
+         */
+        int r = 0, c = cols - 1;
+        while(r < rows && c >= 0) {
+            if(matrix[r][c] == target) return true;
+
+            if(matrix[r][c] < target) r++;
+            else c--;
+        }
+        return false;
+    }
+
+    public boolean searchMatrix2(int[][] matrix, int target) {
 
         int M = matrix.length;
         for(int i = 0;  i < M; i++) {
