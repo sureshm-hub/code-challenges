e8bec434ff0c2e77f0d725877f02434499315834
sures <suresh.malladi@gmail.com>
2026-01-26 02:40:54 -0500

    refactoring

--- src/main/java/proj/hobby/dsa/math/BulbSwitcher.java
+++ src/main/java/proj/hobby/dsa/math/BulbSwitcher.java
@@ -13,6 +13,15 @@ public class BulbSwitcher {
         int on = 0;
         int max = (int) Math.sqrt(n);
         for(int i = 1; i <= max; i++) {
+            /**
+             *
+             * Don’t need to simulate anything.
+             *
+             * After k rounds, bulb i has been toggled once per divisor of i.
+             * Only perfect squares have an odd number of divisors → only those end up ON.
+             * So the answer is “how many perfect squares ≤ n”, i.e. ⌊√n⌋.
+             *
+             */
             // boolean state = true;
             // for(int j = 2; j <= n; j++) {
             //     if(i % j == 0) {
